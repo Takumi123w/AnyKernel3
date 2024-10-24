ff8e327504b5b042d06132918a2d096c915dd230
Takumi123w <166905091+Takumi123w@users.noreply.github.com>
2024-10-25 03:05:00 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel by Aqua Lyudmila (FranklinAzriel) @ aquacommunity.org
+kernel.string=Kernel made by TakuruKagami from t.me/a01coredev
 do.devicecheck=0
 do.modules=0
 do.systemless=1
